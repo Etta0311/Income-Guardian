f8b3a23abb7969501e911ac67450720074f77e29
ettaho <etta0311031@gmail.com>
2022-02-03 20:05:45 +1100

    mutation login signup added

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,29 +3,60 @@ const { User } = require("../models");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
+  Query: {
+    users: async () => {
+      const userData = await User.find({}).populate("expences");
+      console.log(userData);
 
-      users: async () => {
-        const userData = await User.find({}).populate("expences");
-        console.log(userData);
+      return userData;
+    },
+
+    userById: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("expences");
+      }
+      throw new AuthenticationError("Login to continue.");
+    },
+  },
+  Mutation: {
+    signUp: async (parent, args) => {
+      let finduser = await User.findOne({ email });
+      //If user not found then show error
+      if (finduser) {
+        throw new Error("User Already Exists! Please use antoher email to register.");
+      }
+
+      let findusername = await User.findOne({ username });
+
+      //If username has been taken then show error
+      if (findusername) {
+        throw new Error("Username Already Taken! Please use antoher username to register.");
+      }
+
+      let user = await User.create(args.data);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+
+    login: async (parent, { username, password }) => {
+        const user = await User.findOne({ username });
+        const correctPw = await user.isCorrectPassword(password);
   
-        return userData;
-      },
-
-      userById: async (parent, args, context) => {
-        if (context.user) {
-          return User.findOne({ _id: context.user._id })
-            .select("-__v -password")
-            .populate("expences")
+        // If email or password is incorrect, throw error
+        if (!user || !correctPw) {
+          throw new AuthenticationError("Incorrect username or passwords.");
         }
-        throw new AuthenticationError("Login to continue.");
+  
+        // When logged in
+        const token = signToken(user);
+        return { token, user };
       },
-    },
-    Mutation: {
-        signUp: async (parent, { userInput }) => {
-            let { username, email, password, confirmPassword } = userInput;
 
-    },
+      
+  },
 };
 
 module.exports = resolvers;
