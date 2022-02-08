e78e7b91b841b2c6f4ed91a7751838afee7f1e08
ettaho <etta0311031@gmail.com>
2022-02-08 15:18:51 +1100

    expenserecord

--- client/src/components/expenserecord.js
+++ client/src/components/expenserecord.js
@@ -2,19 +2,29 @@ import React from "react";
 import { useMutation } from "@apollo/client";
 import { DELETE_EXPENCE } from "../utils/mutation";
 import { QUERY_USER } from "../utils/queries";
-import { Box, Center, Text, Image, Heading, Button } from "@chakra-ui/react";
-import {
-  Table,
-  Thead,
-  Tbody,
-  Tr,
-  Th,
-  Td,
-} from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
+import { Table, Thead, Tbody, Tr, Th, Td } from "@chakra-ui/react";
 // import UpdatePost from "./modals/ModalUpdatePost";
 
 // Render record
-const Recordlist = ({ title, transactionAmount }) => {
+const Recordlist = ({ Expenses }) => {
+  // const [updateExpense] = useMutation(UPDATE_EXPENSE, {
+  //     refetchQueries: [QUERY_USER],
+  //   });
+  // const Updates = async (Id) => {
+  //     try {
+  //       const data = await updateExpense({
+  //         variables: {
+  //           id: Id,
+  //         },
+  //       });
+  //     console.log(date);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+
   const [removeExpense] = useMutation(DELETE_EXPENCE, {
     refetchQueries: [{ query: QUERY_USER }],
   });
@@ -30,8 +40,19 @@ const Recordlist = ({ title, transactionAmount }) => {
     }
   };
 
+//   if (!Expenses.length) {
+//     return (
+//       <Box>
+//           <Center>
+//               <Heading> No Expense Record</Heading>
+//           </Center>
+//       </Box>
+//     );
+//   }
+
   return (
     <Box m={12}>
+        <Center>
       <Table size="lg" variant="striped" colorScheme="teal">
         <Thead>
           <Tr>
@@ -50,6 +71,7 @@ const Recordlist = ({ title, transactionAmount }) => {
           </Tr>
         </Tbody>
       </Table>
+      </Center>
     </Box>
   );
 };
